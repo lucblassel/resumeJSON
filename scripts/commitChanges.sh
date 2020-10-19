d4f81ba3fc78b45cb8092f5964cd1faeb8e5d154
Luc Blassel <luc.blassel@gmail.com>
2020-10-19 19:01:34 +0200

    adding conditional commit to workflow

---
+++ scripts/commitChanges.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+filename=$1
+
+# generate commit message
+if [ ${filename: -4} == ".tex" ]; then
+    message="creating .tex"
+elif [ ${filename: -4} == ".pdf" ]; then
+    message="creating .pdf"
+else
+    echo "error wrong input file"
+    exit 0
+fi
+
+# commit and push if file changed
+if git diff --exit-code -s $filename; then
+    echo "Nothing to commit."
+    exit 1
+else
+    git config --global user.name 'Luc Blassel'
+    git config --global user.email 'luc.blassel@gmail.com'
+    git add $filename
+    git commit -m $message
+    git push
+fi
+
+exit 1
